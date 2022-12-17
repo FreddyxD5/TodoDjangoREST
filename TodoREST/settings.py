e7dbf5889ad1f4a8362cd5d1362788d8de7a8562
FreddyxD5 <freddyxd5@gmail.com>
2022-12-17 00:33:47 -0500

    Añadido scope de 1000 peticiones/day para PaymentViewSet and 500 to ServiceViewSet, django_filters added to paymentViewSet

--- TodoREST/settings.py
+++ TodoREST/settings.py
@@ -117,7 +117,9 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.ScopedRateThrottle',
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'get': '1/day',
+        'get': '1000/day',
+        'services':'500/day',
+        'payments':'1000/day',
     }
 }
 
@@ -145,7 +147,39 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+from datetime import timedelta
 
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Token',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
--- payment/api.py
+++ payment/api.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from django_filters import rest_framework as filters
 from payment.models  import Payment, Service
 from payment.serializers import PaymentSerializer, ServiceSerializer
 from rest_framework.permissions import IsAuthenticated
@@ -8,8 +9,14 @@ class ServiceViewSet(viewsets.ModelViewSet):
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
     permission_classes=[IsAuthenticated]
+    throttle_scope = 'services'
 
 class PaymentViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
     permission_classes=[IsAuthenticated] 
+
+    filter_backends = [filters.DjangoFilterBackend]    
+    filterset_fields = ('user__id','payment_date', 'service',)
+
+    throttle_scope = 'payments'
